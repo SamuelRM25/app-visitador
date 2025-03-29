9e0996ca375219e0059972a013b5ce3ba4b7e27a
SamuelRM25 <124316227+SamuelRM25@users.noreply.github.com>
2025-03-29 14:12:46 -0600

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,10 +5,10 @@ dotenv.config();
 
 // Configuración de la conexión a la base de datos
 const pool = mysql.createPool({
-  host: process.env.MYSQL_ADDON_HOST || 'b3ihaoqkmoah8pl89tsr-mysql.services.clever-cloud.com',
-  user: process.env.MYSQL_ADDON_USER || 'u8geiftmkdmlxwhf',
-  password: process.env.MYSQL_ADDON_PASSWORD || 'fbxQNWPTZ9aFBUAcf0mJ',
-  database: process.env.MYSQL_ADDON_DB || 'b3ihaoqkmoah8pl89tsr',
+  host: process.env.MYSQL_ADDON_HOST || 'bfpwrmhy4dzlk0mpxsrb-mysql.services.clever-cloud.com',
+  user: process.env.MYSQL_ADDON_USER || 'udmfyptt5vjnpyao',
+  password: process.env.MYSQL_ADDON_PASSWORD || 'zoN6Mu8xYDKblz35ZpGz',
+  database: process.env.MYSQL_ADDON_DB || 'bfpwrmhy4dzlk0mpxsrb',
   port: process.env.MYSQL_ADDON_PORT || '3306',
   waitForConnections: true,
   connectionLimit: 10,
